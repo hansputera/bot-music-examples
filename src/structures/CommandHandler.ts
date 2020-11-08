02ee7a882d206b7ca7abd5eca75697399066c0ee
hansputera <hanifdwyputrasembiring@gmail.com>
2020-11-08 12:00:02 +0700

    ! instead of as string

--- src/structures/CommandHandler.ts
+++ src/structures/CommandHandler.ts
@@ -7,7 +7,7 @@ export default class CommandHandler {
         const args = message.content.slice(this.client.config.PREFIX.length).trim().split(/ +/g);
         const command = args.shift()!.toLowerCase();
 
-        const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command) as string);
+        const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command)!);
         if (cmd!.guildOnly && message.channel.type === "dm") return;
         if (cmd!.ownerOnly && !this.client.config.owners.includes(message.author.id)) return message.reply("Only developers can execute this command!");
         cmd!.execute(message, args);
